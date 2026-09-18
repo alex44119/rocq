bb00932c7f5fbce13995ab786cbc85b0e0e2065e
Alex Martinez Rivera <alexmr@kth.se>
2026-09-18 17:27:20 +0200

    Revert wip commit of TDiazT

--- tactics/elimschemes.mli
+++ tactics/elimschemes.mli
@@ -29,8 +29,6 @@ val pseudo_sort_quality_for_elim : inductive -> Declarations.one_inductive_body 
 val default_case_analysis_dependence : env -> inductive -> bool
 
 (** Induction/recursion schemes *)
-val poly_dep : individual scheme_kind
-
 val elim_scheme : dep:bool -> to_kind:UnivGen.QualityOrSet.t -> individual scheme_kind
 
 (** Case analysis schemes *)
